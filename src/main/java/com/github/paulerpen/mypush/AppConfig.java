889de4b5605d170c4413a74acf24fe99a979a4ff
Paulemeier <paulerpenstein@yahoo.de>
2018-06-04 14:21:01 +0200

    functionality working no styling

--- src/main/java/com/github/paulerpen/mypush/AppConfig.java
+++ src/main/java/com/github/paulerpen/mypush/AppConfig.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @Configuration
 public class AppConfig {
 	
@@ -18,5 +21,9 @@ public class AppConfig {
 		StorageProperties ret = new StorageProperties();
 		ret.setLocation(env.getProperty("mypush.paths.uploaded"));
 		return ret;
+	}
+	@Bean
+	public CommonsMultipartResolver filterMultipartResolver(){
+	    return new CommonsMultipartResolver();
 	}
 }
--- src/main/java/com/github/paulerpen/mypush/UploadController.java
+++ src/main/java/com/github/paulerpen/mypush/UploadController.java
@@ -15,18 +15,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.github.paulerpen.mypush.storage.StorageException;
 import com.github.paulerpen.mypush.storage.StorageFileNotFoundException;
 import com.github.paulerpen.mypush.storage.StorageService;
 
 @Controller
 public class UploadController {
-
+	
     private final StorageService storageService;
 
     @Autowired
@@ -34,7 +37,7 @@ public class UploadController {
         this.storageService = storageService;
     }
 
-    @GetMapping("/hub")
+    @RequestMapping(value = "/hub", method = RequestMethod.GET)
     public String listUploadedFiles(Model model,
 			HttpServletResponse response) throws IOException {
     	System.out.println("Went to hub");
@@ -55,14 +58,18 @@ public class UploadController {
                 "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
 
-    @PostMapping("/hub")
+    @RequestMapping(value = "/hub", method = RequestMethod.POST)
     public String handleFileUpload(@RequestParam("file") MultipartFile file,
             RedirectAttributes redirectAttributes) {
     	System.out.println("Went to fileUpload");
-        storageService.store(file);
-        redirectAttributes.addFlashAttribute("message",
-                "You successfully uploaded " + file.getOriginalFilename() + "!");
-
+    	try {
+    		storageService.store(file);
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded " + file.getOriginalFilename() + ".");
+    	}catch(StorageException e) {
+    		redirectAttributes.addFlashAttribute("message", "Your file could not be uploaded.");
+    	}
+        
         return "redirect:/hub";
     }
 
